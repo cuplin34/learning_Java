217484c28075162ee512ed466e127ec9fdfdbb20
cuplin34 <bradycuplin@yahoo.com>
2023-07-12 12:08:26 -0700

    Basic set-up for java and practice problem code

---
+++ Test/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+        int score = 5000;
+        int roundsPlayed = 6;
+
+
+        calcScore(score, roundsPlayed);
+    }
+
+    public static void calcScore(int score, int roundsPlayed){
+        int bonus = 150;
+        int finalScore = score + (roundsPlayed * bonus);
+        System.out.println("Wow you scored " + finalScore + " points");
+    }
+}
--- helloWorld.java
+++
@@ -1,6 +0,0 @@
-//Truly a classic
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello World");
-    }
-}
