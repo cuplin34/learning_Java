3af79470919f255ca9f85e1eb84751ac5b7fec33
cuplin34 <bradycuplin@yahoo.com>
2023-07-14 11:44:03 -0700

    Create time converter which accepts sec and mins

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,16 +1,35 @@
 public class Main {
     public static void main(String[] args){
-        int score = 5000;
-        int roundsPlayed = 6;
+        System.out.println(secondsCalc(102, 136));
+        System.out.println(secondsCalc(89403));
 
 
-        calcScore(score, roundsPlayed);
     }
 
-    public static int calcScore(int score, int roundsPlayed){
-        int bonus = 150;
-        int finalScore = score + (roundsPlayed * bonus);
-        System.out.println("Wow you scored " + finalScore + " points");
-        return finalScore;
+    public static String secondsCalc(int seconds){
+        if (seconds >= 60) {
+            int minutes = seconds / 60;
+            seconds -= minutes * 60;
+            return secondsCalc(seconds, minutes);
+        } else {
+            return seconds + "s";
+        }
+    }
+
+    public static String secondsCalc(int seconds, int minutes){
+        int hours;
+        int interimSeconds;
+        if (minutes >= 60 || seconds >= 60){
+            interimSeconds = seconds /60;
+            minutes += interimSeconds;
+            seconds -= interimSeconds * 60;
+            hours = (minutes / 60);
+            minutes -= hours * 60;
+            return hours + "hrs " + minutes + "mins " + seconds + "s";
+        } else if (minutes <= 0 || seconds <= 0) {
+            return "Please enter a correct time format for seconds and minutes";
+        } else {
+            return "00hrs " + minutes + "mins " + seconds + "s";
+        }
     }
 }
