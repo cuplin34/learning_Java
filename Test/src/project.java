a2a9bd90f05d0429e4635f25f801c71c1792549b
cuplin34 <bradycuplin@yahoo.com>
2023-07-20 17:37:15 -0700

    Creates method to find largest prime factor of a number

--- Test/src/project.java
+++ Test/src/project.java
@@ -1,19 +1,10 @@
+import java.util.Scanner;
+
 public class project {
     public static void main(String[] args) {
-    }
-
-    public static int sumDigits (int number){
-        int digit = 0;
-        if(number < 0 ){
-            return -1;
-        } else if (number <= 9){
-            return number;
-        } else{
-            while(number > 0){
-                digit += number % 10;
-                number = number / 10;
-            }
-            return digit;
-        }
+        String name = System.console().readLine("Hi what's your name? ");
+        System.out.println(name);
+        Scanner sc = new Scanner(System.in);
+        System.out.println(7/3);
     }
 }
